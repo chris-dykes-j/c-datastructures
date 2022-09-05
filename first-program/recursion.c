1af7e3fbdde8e0b0416148eccf659e63a0b6da19
Chris Dykes <chris.t.c.dykes@gmail.com>
2022-09-05 11:03:06 -0400

    Add factorial function

--- first-program/recursion.c
+++ first-program/recursion.c
@@ -9,7 +9,17 @@ int fun(int x) {
 	return 0;
 }
 
+int factorial(int x) {
+	if (x == 1)
+		return 1;
+	else
+		return x * factorial(x - 1);
+}
+
 int main() {
 	// What number will be printed out?
 	printf("%d\n", fun(5));
+	
+	// Factorial test; should be 120.
+	printf("%d\n", factorial(5));
 }
